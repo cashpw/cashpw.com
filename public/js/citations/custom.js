3470204ff2440bb4d7b76359eefb75fc2f9bfe05
Cash Prokop-Weaver <cashbweaver@gmail.com>
2025-11-27 20:59:24 -0800

    refactor: Delete public directory

--- public/js/citations/custom.js
+++
@@ -1,6 +0,0 @@
-const referencesHeading = document.querySelector('#references');
-if (referencesHeading) {
-  referencesHeading.outerHTML =
-    '<hr>'
-    + '<h4 id="references">' + references.innerHTML + '</h4>';
-}
